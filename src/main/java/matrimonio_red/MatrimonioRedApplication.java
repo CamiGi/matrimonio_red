209e7c46c8046aba24d08a4ebc60de1fe518bf23
CamiGi <camilla.gobbi.98@gmail.com>
2023-09-13 16:40:26 +0200

    Initial commit

---
+++ src/main/java/matrimonio_red/MatrimonioRedApplication.java
@@ -0,0 +1,13 @@
+package matrimonio_red;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrimonioRedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatrimonioRedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/matrimonio_red/MatrimonioRedApplicationTests.java
@@ -0,0 +1,13 @@
+package matrimonio_red;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrimonioRedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
